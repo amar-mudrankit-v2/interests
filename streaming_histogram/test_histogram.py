3f434946839600377efc7cec7a3f55c340dc81ca
amar-mudrankit-v2 <114953230+amar-mudrankit-v2@users.noreply.github.com>
2022-10-03 23:34:46 -0700

    More changes

--- streaming_histogram/test_histogram.py
+++ streaming_histogram/test_histogram.py
@@ -8,13 +8,14 @@ def add_values_to_histogram(count, max_buckets):
     for _ in range(n):
         histogram.push_value(random.uniform(-32767.0, 32768.0))
 
-    assert histogram.count_values() == n
+    assert histogram.count() == n
     return histogram
 
 
 def test_basic_histogram():
     histogram = add_values_to_histogram(100000, 15)
     histogram.print()
+    print(histogram.mean())
 
 
 def test_merge_histogram():
@@ -22,7 +23,7 @@ def test_merge_histogram():
     histogram2 = add_values_to_histogram(10000, 10)
     histogram1.merge(histogram2)
     histogram1.print()
-    assert histogram1.count_values() == 11000
+    assert histogram1.count() == 11000
 
 
 def test_list_histogram():
@@ -37,3 +38,12 @@ def test_list_histogram():
     list_histogram.print()
 
     assert stream_histogram == list_histogram
+
+
+def test_same_value():
+    values = [random.uniform(-32767.0, 32768.0) for _ in range(1000)]
+    stream_histogram = StreamingHistogram(max_buckets=10)
+    stream_histogram.push_list(values)
+    stream_histogram.push_list(values)
+    stream_histogram.print()
+    assert stream_histogram.count() == 2000
